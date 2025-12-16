3faa9c11a00e7a23fbca5ce1808f990456ca4062
AceSlaysz <hvlogs136@gmail.com>
2025-12-15 22:04:06 -0500

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-console.log("Welcome to Ace's Mind Palace.");
-
-// Function to toggle the menu on small screens
-function toggleMenu() {
-  const menu = document.querySelector('.menu');
-  menu.classList.toggle('active');
-}
